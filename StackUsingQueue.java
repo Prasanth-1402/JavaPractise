274b06c1539504188cb62a4ce0ff8dfc024002f3
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-17 23:29:51 +0530

    [Prasanth] recovered a Deleted File

--- StackUsingQueue.java
+++ StackUsingQueue.java
@@ -1,57 +1,28 @@
 import java.util.ArrayList;
-import java.util.List;
-
 public class StackUsingQueue {
-    List<Integer> stack ;
-    int size;
+        ArrayList<Integer> queue1;
+        int size;
+        public StackUsingQueue(){
+            this.queue1 = new ArrayList<>();
+            this.size=size; }
 
-    public StackUsingQueue(){                                       // Constructor
-        this.stack = new ArrayList<>();
-    }
-        public boolean Empty(){                                     // Check for stack Emptiness
-        if(stack.isEmpty())
-            return true;
-        return false;
+        private  void enqueue(int n) { push(n); }
+
+        private  void push(int x)    { queue1.add(x); }
+
+        private void pop()           { queue1.remove(this.queue1.size()-1); }
+
+        private  void dequeue()      { pop(); }
+
+        private void display()       { System.out.println(this.queue1); }
+
+        public static void main(String[] args) {
+            StackUsingQueue s = new StackUsingQueue();
+            s.enqueue(1);
+            s.enqueue(2);
+            s.enqueue(4);
+            s.display();
+            s.dequeue();
+            s.display();
         }
-    public void push(int n){                                        //Pushing Value
-        if(n >= 0)
-            stack.add(n);
-        else
-            System.out.println(n +" is a Negative Value,"+" Give a Positive Value");
-    }
-
-    public int pop(){                                               //Popping Value
-        if(!stack.isEmpty()){
-            return stack.remove(0);}
-        return -1;
-    }
-
-    public int top(){                                               //Looking for the Top most Element in the Stack
-        if(!stack.isEmpty()){
-            int length = stack.size()-1;
-            return stack.get(length); }
-        return -1;
-    }
-
-
-    public static void main(String[] args) {                        // 'Main' method
-        StackUsingQueue stack = new StackUsingQueue();
-        System.out.println(stack.Empty());
-        stack.push(3);
-        stack.push(8);
-        stack.push(4);
-        stack.push(7);
-        System.out.println(stack.stack);
-        System.out.println(stack.Empty()+", that it an Empty Stack");
-        System.out.println(stack.top()+" is the Top Element in the stack Right now");
-        System.out.println(stack.pop()+" is Popped Out");
-        System.out.println(stack.top()+" is the Top Element in the stack Right now");
-        System.out.println(stack.pop()+" is Popped Out");
-        stack.push(12);
-        System.out.println(stack.stack);
-        System.out.println(stack.pop()+" is Popped Out");
-        System.out.println(stack.top()+" is the Top Element in the stack Right now");
-        System.out.println(stack.Empty()+", that it an Empty Stack");
-        System.out.println(stack.Empty());
-    }
 }
